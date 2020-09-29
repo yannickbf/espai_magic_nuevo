65e0b9ffa08611b9bb395925ce1834ccf5e98171
= <=>
2020-09-29 13:12:01 +0200

    comiteo con archivo guardar_cambios_edicion.php pasado a while

--- editar_eliminar_entrada/guardar_cambios_edicion.php
+++ editar_eliminar_entrada/guardar_cambios_edicion.php
@@ -21,17 +21,30 @@ if( isset($_POST['guardar_entrada'])){
     $entradas = $doc->documentElement;
 
     //Buscamos la entrada de blog que tenga el mismo id que se nos ha pasado y si tiene el mismo id editamos todo el nodo entrada_blog diciendole la posicion
-    for($i=0;$i<$cuenta_entradas_blog;$i++){
+    $seguir_buscando = true;
+    $i=0;
+    while($seguir_buscando){
         if($entrada_blog[$i]->id == $id){
-            //Guardamos en una variable la entrada a editar y la editamos
+
+            //Cambiamos el contenido del XML con el nuevo contenido editado
             $entradas->getElementsByTagName('entrada_blog')->item($i)->getElementsByTagName('titulo')->item(0)->nodeValue = $titulo;
             $entradas->getElementsByTagName('entrada_blog')->item($i)->getElementsByTagName('descripcion')->item(0)->nodeValue = $descripcion;
 
             //Guardamos los cambios en el archivo xml
             $doc->save('../xml/entradas_blog.xml');
 
-            echo "<h1>Eliminado con exito!!</h1> <a href='index.php'><--Volver</a>";
+            //Mostramos mensaje editado con exito
+            echo "<h1>Editado con exito!!</h1> <a href='index.php'><--Volver</a>";
+
+            //Pasamos $seguir_buscando a false para detener el bucle
+            $seguir_buscando = false;
+        }
+        else{
+            $i++;
         }
     }
+
+
+    
 }
 ?>
