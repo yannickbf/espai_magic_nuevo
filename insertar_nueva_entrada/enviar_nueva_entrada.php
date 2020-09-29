19f0c43c0dd7b5c627a37bbe4c9be36aac33dfaa
= <=>
2020-09-29 13:46:23 +0200

    subo los con la comprobacion de que hay algo escrito cuando envias nueva entrada. Ahora probare lastchild

--- insertar_nueva_entrada/enviar_nueva_entrada.php
+++ insertar_nueva_entrada/enviar_nueva_entrada.php
@@ -2,46 +2,57 @@
     // Agregar info a XML
     // Se ejecuta cuando enviamos por $_POST en la misma pagina
     if( isset($_POST['mv'])){
-        //Cambiamos la zona horaria
-        date_default_timezone_set("Europe/Madrid");
-        //Obtenemos la fecha con date y la formateamos para que se muestre como queremos
-        $hoy = date("j/n/Y, g:i a"); 
 
         //Guardamos el titulo y descripcion obtenidos por $_POST
         $Titulo = $_POST['Titulo'];
         $Descripcion = $_POST['Descripcion'];
 
-        //Cargamos el XML desde un archivo xml
-        $xml = simplexml_load_file('../xml/entradas_blog.xml');
+        //Si no han rellenado algun campo mostramos mensaje
+        if($Titulo == "" || $Descripcion == ""){
+            echo "<h1>Debes rellenar un titulo y una descripcion</h1>";
+            echo '<a href="javascript:history.back()"> <--Volver a edicion</a>';
+        }
+        //Si han rellenado insertamos la entrada con un id +1 que el ultimo
+        else{
+            //Cambiamos la zona horaria
+            date_default_timezone_set("Europe/Madrid");
+            //Obtenemos la fecha con date y la formateamos para que se muestre como queremos
+            $hoy = date("j/n/Y, g:i a"); 
 
-        //ATENCION he intentado obtener el ultimo entrada_blog con [-1] para cojer directamente el ultimo nodo y con lastChild.
-        //No lo he conseguido y no encuentro info en internet, asi que he obtenido la ultima posicion de la siguiente manera:
+            //Cargamos el XML desde un archivo xml
+            $xml = simplexml_load_file('../xml/entradas_blog.xml');
 
-        //Contamos las entradas del nodo entrada_blog
-        $contar_entradas_blog = $xml->entrada_blog->count();
+            //ATENCION he intentado obtener el ultimo entrada_blog con [-1] para cojer directamente el ultimo nodo y con lastChild.
+            //No lo he conseguido y no encuentro info en internet, asi que he obtenido la ultima posicion de la siguiente manera:
 
-        //Le restamos 1 para obtener la posicion de la ultima entrada
-        $contar_entradas_blog--;
+            //Contamos las entradas del nodo entrada_blog
+            $contar_entradas_blog = $xml->entrada_blog->count();
 
-        //Guardamos el id de la ultima entrada
-        $id_ultimo_elemento = $xml->entrada_blog[$contar_entradas_blog]->id;
+            //Le restamos 1 para obtener la posicion de la ultima entrada
+            $contar_entradas_blog--;
 
-        //Guardamos el id de la nueva entrada, para ello le sumamos +1 a la ultima entrada
-        $id_entrada_blog = $id_ultimo_elemento+1;
+            //Guardamos el id de la ultima entrada
+            $id_ultimo_elemento = $xml->entrada_blog[$contar_entradas_blog]->id;
+
+            //Guardamos el id de la nueva entrada, para ello le sumamos +1 a la ultima entrada
+            $id_entrada_blog = $id_ultimo_elemento+1;
+            
+            //Creamos un nodo entrada_blog y lo guardamos en la variable $entrada_blog
+            $entrada_blog = $xml->addChild('entrada_blog');
+
+            //Creamos los subnodos con su informacion correspondiente que hemos obtenido
+            $entrada_blog->addChild('id',$id_entrada_blog);
+            $entrada_blog->addChild('fecha',$hoy);
+            $entrada_blog->addChild('titulo',$Titulo);
+            $entrada_blog->addChild('descripcion',$Descripcion);
+
+            //Guardamos la nueva entrada en el arxivo XML
+            file_put_contents('../xml/entradas_blog.xml',$xml->asXML());
+
+            //Mostramos mensaje confirmando que se ha subido la entrada
+            echo "<h1>Subido con exito!!</h1> <a href='index.html'><--Volver</a>";
+        }
+
         
-        //Creamos un nodo entrada_blog y lo guardamos en la variable $entrada_blog
-        $entrada_blog = $xml->addChild('entrada_blog');
-
-        //Creamos los subnodos con su informacion correspondiente que hemos obtenido
-        $entrada_blog->addChild('id',$id_entrada_blog);
-        $entrada_blog->addChild('fecha',$hoy);
-        $entrada_blog->addChild('titulo',$Titulo);
-        $entrada_blog->addChild('descripcion',$Descripcion);
-
-        //Guardamos la nueva entrada en el arxivo XML
-        file_put_contents('../xml/entradas_blog.xml',$xml->asXML());
-
-        //Mostramos mensaje confirmando que se ha subido la entrada
-        echo "<h1>Subido con exito!!</h1> <a href='index.html'><--Volver</a>";
     }
 ?>
